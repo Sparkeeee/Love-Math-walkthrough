c4c7ab931585f91f3103331dd53e7e5d6b38039f
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-01-30 17:16:02 +0000

    add functionality for checking user answers and responding

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
   for (let button of buttons) {
       button.addEventListener("click", function() {
           if (this.getAttribute("data-type") === "submit") {
-              alert("You clicked Submit!");
+              checkAnswer();
           } else {
               let gameType = this.getAttribute("data-type");
               runGame(gameType);
@@ -36,13 +36,34 @@ function runGame(gameType) {
       throw `Unknow game type: ${gameType}. Aborting!`;
     }
 }
-
+/**
+ *  
+ */
 function checkAnswer() {
-
+    let userAnswer = parseInt(getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) {
+      alert("Hey you got it right! :D" );
+    } else {
+        alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+    runGame(calculatedAnswer[1]);
+  
 }
 
 function calculateCorrectAnswer() {
 
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+      return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
@@ -54,7 +75,7 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-  
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
