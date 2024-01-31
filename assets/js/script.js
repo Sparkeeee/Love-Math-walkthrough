4b0a5f58591d245f60531536617c376d7f51cfe4
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-01-31 16:52:58 +0000

    Final touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,7 @@ function runGame(gameType) {
   // Creates 2 random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25);
   let num2 = Math.floor(Math.random() * 25);
+  let num = Math.floor(Math.random() * 100) + 1;
 
     if (gameType === "addition"){
       displayAdditionQuestion(num1, num2);
@@ -46,6 +47,8 @@ function runGame(gameType) {
       displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract"){
       displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division"){
+      displayDivisionQuestion(num1, num2);
     }else {
       alert(`Unknown game type: ${gameType}`);
       throw `Unknow game type: ${gameType}. Aborting!`;
@@ -82,6 +85,8 @@ function calculateCorrectAnswer() {
       return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
       return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+      return [Math.round(operand1 / operand2), "division"];
     }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -123,4 +128,11 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+  operand1 = operand1 * operand2;
+  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent =  operand1 > operand2 ? operand2 : operand1;
+  document.getElementById('operator').textContent = "/";
 }
